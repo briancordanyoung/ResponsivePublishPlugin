85c27439c9d39df801f02541680ab1286143685d
Brian Cordan Young <brian@cognoa.com>
2022-05-29 18:57:14 -0700

    Update to swift 5.6

--- Package.swift
+++ Package.swift
@@ -1,24 +1,41 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "ResponsivePublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "ResponsivePublishPlugin",
             targets: ["ResponsivePublishPlugin"]),
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
-        .package(name: "SwiftGD", url: "https://github.com/briancordanyoung/SwiftGD.git", from: "2.0.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(url: "https://github.com/briancordanyoung/SwiftGD.git", from: "2.0.0"),
     ],
     targets: [
         .target(
             name: "ResponsivePublishPlugin",
-            dependencies: ["Publish", "SwiftGD"]),
+            dependencies: [
+                .product(name: "Publish", package: "publish"),
+                "SwiftGD"
+            ]
+        ),
         .testTarget(
             name: "ResponsivePublishPluginTests",
-            dependencies: ["ResponsivePublishPlugin"]),
+            dependencies: ["ResponsivePublishPlugin"],
+            resources: [
+                .copy("Expected/index-expected.html"),
+                .copy("Expected/styles-expected-no-rewrite.css"),
+                .copy("Expected/styles-expected.css"),
+                .copy("Resources/css/styles-no-rewrite.css"),
+                .copy("Resources/css/styles.css"),
+                .copy("Resources/img/icons/favicon.ico"),
+                .copy("Resources/img/subfolder/sub-background.jpg"),
+                .copy("Resources/index.html"),
+                .copy("Resources/img/background.jpg"),
+            ]
+        ),
     ]
 )
